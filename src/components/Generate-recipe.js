f5b8a6ebd517ebcb675165b8efe7febbaa87b16c
Chhaya-Verma <chhayav300@gmail.com>
2024-12-19 16:53:17 +0530

    Add edit feature

--- src/components/Generate-recipe.js
+++ src/components/Generate-recipe.js
@@ -2,52 +2,53 @@ import React, { useState } from "react";
 import "./Generate-recipe.css";
 
 function RecipeGenerator() {
-  const [input, setInput] = useState("");
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredient, setIngredient] = useState(""); 
+  const [ingredientList, setIngredientList] = useState([]); 
 
+  
   function addIngredient() {
-    if (input.trim() !== "" && !ingredients.includes(input)) {
-      setIngredients([...ingredients, input]);
-      setInput("");
+    if (ingredient.trim() !== "" && !ingredientList.includes(ingredient)) {
+      setIngredientList([...ingredientList, ingredient]); 
+      setIngredient("");
     } else {
-      alert("Enter a valid or new ingredient.");
+      alert("Please enter a valid or new ingredient."); 
     }
   }
 
+  
+  function editIngredient(index) {
+    const edited = prompt("Edit the ingredient:", ingredientList[index]);
+    if (edited && edited.trim() !== "") {
+      const updatedList = ingredientList.slice(); 
+      updatedList[index] = edited; 
+      setIngredientList(updatedList); 
+    } else {
+      alert("Please enter a valid ingredient."); 
+    }
+  }
+
+  
   function deleteIngredient(index) {
-    const updated = ingredients.filter((_, i) => i !== index);
-    setIngredients(updated);
+    const updatedList = ingredientList.filter((item, i) => i !== index); 
+    setIngredientList(updatedList); 
   }
 
   return (
     <div className="recipe-generator-form">
       <h1>Recipe Generator</h1>
-      <div className="ingredient-div">
+      <div>
         <h3>Ingredients:</h3>
-        <input
-          type="text"
-          value={input}
-          placeholder="Enter ingredient"
-          className="ingredient-input"
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button className="ingredient-add-btn" onClick={addIngredient}>
-          Add Ingredient
-        </button>
-        <ul className="ingredient-list">
-          {ingredients.map((item, index) => (
+        <input type="text" value={ingredient}  placeholder="Enter ingredient " onChange={(e) => setIngredient(e.target.value)}  />
+          <button onClick={addIngredient}>Add Ingredient</button>
+        <ul>
+          {ingredientList.map((item, index) => (
             <li key={index}>
-              {item} 
+              {item}
+              <button onClick={() => editIngredient(index)}>Edit</button>
               <button onClick={() => deleteIngredient(index)}>Delete</button>
             </li>
           ))}
         </ul>
-        <button
-          className="get-recipe-btn"
-          onClick={() => alert("Fetching recipes...")}
-        >
-          Get Recipes
-        </button>
       </div>
     </div>
   );
